4171755fa34c83b75ba06882c12a39ada15e5ef8
5P2RS5 <rhfoqkqfken@gmail.com>
2022-04-11 18:05:02 +0900

    카메라 회전 상하
    카메라 회전 상하 구현 (범위구현은 아직 안함)

--- ToyStory/Assets/5_Scirpts/CameraManager.cs
+++ ToyStory/Assets/5_Scirpts/CameraManager.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Transform player;
-
-    public Vector3 offset;
-
+    //public Transform player;
+    //public Vector3 offset;
+    [SerializeField] float speed;
+    Quaternion initCameraRotate;
     Animator _animator;
+
+    float h;
+
+    void awake()
+    {
+        initCameraRotate = transform.rotation;
+    }
+    
     void Update()
     {
-        transform.position = player.position + offset;
-        _animator = GetComponent<Animator>();
+        Debug.Log(Convert.ToInt32(h));
+        CameraRotate();
+        // transform.position = player.position + offset;
+        // _animator = GetComponent<Animator>();
     }
 
     public void Anim()
     {
         _animator.SetTrigger("doCamera");
+    }
+
+    void CameraRotate() // 카메라 회전
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            if(h < 0.5)
+                h += 0.1f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (h > -0.5)
+                h -= 0.1f;
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            transform.rotation = initCameraRotate;
+        }
+        h = speed * h * Time.deltaTime;
+        transform.Rotate(Vector3.right * h);
     }
 }
