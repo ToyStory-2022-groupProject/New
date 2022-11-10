bfadf846f04078521df96de5eef3aa054d61e3a2
5P2RS5 <90513347+5P2RS5@users.noreply.github.com>
2022-11-10 17:02:23 +0900

    .
    .

--- ToyStory/Assets/5_Scirpts/CheckSightEditor.cs
+++ ToyStory/Assets/5_Scirpts/CheckSightEditor.cs
@@ -1,27 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-[CustomEditor(typeof(CheckSight))]
-
-public class CheckSightEditor : Editor
-{
-    void OnSceneGUI()
-    {
-        CheckSight fow = (CheckSight)target;
-        Handles.color = Color.white;
-        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
-        Vector3 viewAngleA = fow.DirFromAngle (-fow.viewAngle / 2, false);
-        Vector3 viewAngleB = fow.DirFromAngle (fow.viewAngle / 2, false);
-
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
-
-        Handles.color = Color.red;
-        foreach (Transform visibleTarget in fow.visibleTarget) 
-        {
-            Handles.DrawLine(fow.transform.position, visibleTarget.position);
-        }
-        
-    }
-}
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// #if UNITY_EDITOR
+// using UnityEditor;
+// #endif
+// // using UnityEditor;
+// // [CustomEditor(typeof(CheckSight))]
+//
+// public class CheckSightEditor : Editor
+// {
+//     void OnSceneGUI()
+//     {
+//         CheckSight fow = (CheckSight)target;
+//         Handles.color = Color.white;
+//         Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+//         Vector3 viewAngleA = fow.DirFromAngle (-fow.viewAngle / 2, false);
+//         Vector3 viewAngleB = fow.DirFromAngle (fow.viewAngle / 2, false);
+//
+//         Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+//         Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+//
+//         Handles.color = Color.red;
+//         foreach (Transform visibleTarget in fow.visibleTarget) 
+//         {
+//             Handles.DrawLine(fow.transform.position, visibleTarget.position);
+//         }
+//         
+//     }
+// }
